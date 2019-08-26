2c1722b5315ee87e1452ccec366595913e2417e3
Ellah Toren <ellahtoren@gmail.com>
2019-08-26 13:13:52 +0300

    Updates

---
+++ precache-manifest.12775032499ba3b36573efce3a4fd27d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42412d7b77d58a2bc945130313061247",
+    "url": "/mister-bitcoin/index.html"
+  },
+  {
+    "revision": "b3e6794f97b8351cc42d",
+    "url": "/mister-bitcoin/static/css/main.6f16ee77.chunk.css"
+  },
+  {
+    "revision": "02eb87963228737f9471",
+    "url": "/mister-bitcoin/static/js/2.32ea9312.chunk.js"
+  },
+  {
+    "revision": "b3e6794f97b8351cc42d",
+    "url": "/mister-bitcoin/static/js/main.c6920ea4.chunk.js"
+  },
+  {
+    "revision": "ce25db08799b3563d6dd",
+    "url": "/mister-bitcoin/static/js/runtime~main.cfae0b78.js"
+  },
+  {
+    "revision": "0e080ebc77eced54e92efbf4a9724f09",
+    "url": "/mister-bitcoin/static/media/TitilliumWeb-Light.0e080ebc.ttf"
+  }
+]);
